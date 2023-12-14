849115c8bc23c4d3f5349784dba93b37d5e39f80
afetahi <fetahiarmend@gmail.com>
2023-12-14 11:52:54 +0100

    instanciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-# -------------------class ------------------------
+# ------------------- class ------------------------
 class Patients ():
     def __init__(self,nom,maladie,etat,argent=float,poche=[]):
         self.nom = nom
@@ -7,6 +7,11 @@ class Patients ():
         self.argent = argent 
         self.etat = etat 
         self.poche = poche
+
+class Docteur (Patients):
+    def __init__(self, nom, maladie, etat, argent=float, poche=[]):
+        super().__init__(nom, maladie, etat, argent, poche)
+        
         
     def se_deplacer(self):
         pass
@@ -19,5 +24,12 @@ class Lieu ():
         self.nom = nom
         self.salle = salle 
         
-
+        
+# ------------------ Instanciation ----------------
     
+ben = Patients("ben","mal indenté","malade",100)
+optimus = Patients("optimus","unsave","malade",200)
+sangoku = Patients("sangoku","404","malade",80)
+darthvader = Patients("darthvader","azamatique","malade",110)
+semicolon = Patients("semicolon","syntaxError","malade",60)
+docteur = Docteur("docteur","bonne santé","sain", 20000)
