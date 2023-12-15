0a570c1327f5e635edbdc455c50787b1afa04e49
afetahi <fetahiarmend@gmail.com>
2023-12-15 13:46:45 +0100

    création des fonction display

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import threading
+from tabulate import tabulate
+
 # ------------------- class ------------------------
 class Patients ():
     def __init__(self,nom,maladie,etat,argent=float,poche=[]):
@@ -10,14 +12,16 @@ class Patients ():
         self.poche = poche
         
     def se_deplacer(self,_lieu_depart,_lieu_arrive):
-        print(f"{self.nom} : je me trouve à {_lieu_depart} aller cherche mes medocs à la {_lieu_arrive} ")
+        print(f"{self.nom} : je me trouve à {_lieu_depart} et je vais aller cherche mes medocs à la {_lieu_arrive} ")
         _lieu_depart.salle.remove(self)
         _lieu_arrive.salle.append(self)
         
     def prendre_payer(self,_pharmacie,_grille,):
         print(f"{self} : bonjour, je suis venu chercher mon traitement , j'ai {self.maladie}")
+        
         for i in _grille :
             if self.maladie == i.maladie :
+                print(display_traitment())
                 print(f"{_pharmacie.salle[0]} : Bonjour, je vois, je vais vous donner votre médoc qui est {i.nom}")
                 print(f"{_pharmacie.salle[0]} : ca vous fera {i.prix} € ")
                 
@@ -29,7 +33,7 @@ class Patients ():
                     print(f"{_pharmacie.salle[0]} : Merci,passez une bonne journée")
                     self.argent -=i.prix
                     _pharmacie.caisse +=i.prix  
-                       
+
                     self.poche.append(i)
                     self.etat = "entrain de se soigner"
 
@@ -41,9 +45,6 @@ class Chat():
         self.nom = nom
     def __repr__(self):
         return self.nom
-
-    def miauler(self):
-        pass
 class Docteur (Patients):
     def __init__(self, nom, maladie, etat, argent=float, poche=[]):
         super().__init__(nom, maladie, etat, argent, poche)
@@ -56,9 +57,11 @@ class Docteur (Patients):
         
     def diagnostic(self,_cabinet,_patient):
         print(f"{docteur} : je vais vous diagnostiquer ! ")
-        print(f"{_cabinet.patient_in} : d'accord docteur !")
+        print(f"{_cabinet.patient_in[0]} : d'accord docteur !")
         print("quelques minutes plus tard ...")
+        print(display_diagnostic())
         print(f"{docteur} : je vois que vous avez une : {_patient.maladie}")
+        _cabinet.diagnostic = _patient.maladie
         
     def get_payed (self,_patient):
         if _patient.argent < 50 :
@@ -68,7 +71,7 @@ class Docteur (Patients):
         else:
             print(f"{docteur} : ca vous fera 50 €")
             self.argent += 50
-            print(self.argent)
+            _patient.argent -=50
     
     def prescrire (self,_patient,_grille):
         for i in _grille:
@@ -76,12 +79,15 @@ class Docteur (Patients):
                 print(f"{docteur} : je vais vous prescrire pour votre traitement   : {i.nom}")
                 _patient.etat = "en traitement"
                 
+                
     def sortir(self,_cabinet,_patient,_attente):
         print(f"{docteur} : Bon je pense que nous en avons fini, je vous souhaite bon rétablissement et oubliez pas de prendre votre traitement à la pharmacie ! ")
         _cabinet.salle.remove(_patient)
         _cabinet.patient_out.append(_patient)
         _cabinet.patient_in.remove(_patient)
         _attente.salle.append(_patient)
+        print(dislay_cabinet())
+        _cabinet.diagnostic = ""
 
     def __repr__(self):
         return self.nom
@@ -94,7 +100,7 @@ class Lieu ():
     def __repr__(self):
         return self.nom
 class Cabinet (Lieu):
-    def __init__(self, nom,diagnostic ,salle=[],patient_in =[],patient_out = []):
+    def __init__(self, nom,diagnostic =[] ,salle=[],patient_in =[],patient_out = []):
         super().__init__(nom, salle)
         self.diagnostic = diagnostic
         self.patient_in = patient_in
@@ -124,11 +130,12 @@ class Medoc():
 ben = Patients("ben","mal indenté","malade",100)
 optimus = Patients("optimus","unsave","malade",200)
 sangoku = Patients("sangoku","404","malade",80)
-darthvader = Patients("darthvader","azamatique","malade",110)
+darthvader = Patients("darthvader","azmatique","malade",110)
 semicolon = Patients("semicolon","syntaxError","malade",60)
 chat = Chat("chat")
 docteur = Docteur("docteur","bonne santé","sain", 20000)
 pharmacien = Patients("pharmacien","","bonne santé",)
+
 
 patients = [ben,optimus,sangoku,darthvader,semicolon]
 
@@ -147,33 +154,95 @@ cabinet = Cabinet("cabinet ","",[docteur,chat])
 pharmacie = Pharmacie("pharmacie",grille,100,["pharmacienne"])
 cimetierre = Lieu("cimetière",["Fossoyeur"])
 
-# fonction miauler 
-def miauler ():
-    for i in range (100):
-        time.sleep(2)
-        print("*miaouwwww*")
+# # fonction miauler 
+# def miauler ():
+#     for i in range (100):
+#         time.sleep(2)
+#         print("*miaouwwww*")
 
-#chats :
-miauler = threading.Thread(target= miauler)
-miauler.start()
+# #chats :
+# miauler = threading.Thread(target= miauler)
+# miauler.start()
+def display_patient():
+    header = [["nom","maladie","poche","etats","argent"]]
+    for i in patients:
+        content=[i.nom,i.maladie,i.poche,i.etat,i.argent]
+        header.append(content)
+        
+    table = tabulate(header,headers='firstrow',tablefmt='grid')
+    return table
+    
+    
+def dislay_cabinet():
+    header = [["nom","maladie","cabinet","Patient In","Patient ou "]]
+    content=[cabinet.nom,cabinet.diagnostic,cabinet.salle,cabinet.patient_in,cabinet.patient_out]
+    header.append(content)
+    
+    ds_cabinet = tabulate(header,headers='firstrow',tablefmt='grid')
+    return ds_cabinet
+    
+def display_diagnostic ():
+    header = [["Maladie","Traitemnts"]]
+    for i in grille :
+        content= [i.nom,i.maladie]
+        header.append(content)
+    ds_diagnos = tabulate (header,headers='firstrow',tablefmt='grid')
+    return ds_diagnos
+
+def display_traitment ():
+    header = [["Traitemnts","Prix"]]
+    for i in grille :
+        content= [i.maladie,i.prix]
+        header.append(content)
+    ds_diagnos = tabulate (header,headers='firstrow',tablefmt='grid')
+    return ds_diagnos
 
 
-print(salle_attente.salle)
-print(cabinet.salle)
-docteur.recevoir(cabinet,salle_attente,sangoku)
-print(salle_attente.salle)
-print(cabinet.salle)
-docteur.diagnostic(cabinet,sangoku)
-docteur.get_payed(sangoku)
-docteur.prescrire(sangoku,grille)
+def medecin (_cabinet,_salle,_patient,_grille,_pharmacie):
+    print(display_patient())
+    while True :
+        if len(_salle.salle) != 0 :
 
-docteur.sortir(cabinet,sangoku,salle_attente)
-sangoku.se_deplacer(salle_attente,pharmacie)
-print(pharmacie.caisse,sangoku.argent)
-print(sangoku.poche)
-sangoku.prendre_payer(pharmacie,grille)
-print(pharmacie.caisse,sangoku.argent)
-print(sangoku.poche)
+            _patient = str(input(f"Entrez le patient que vous voulez faire rentrer dans le cabinet : {_salle.salle} :"))
+            for i in patients:
+                if _patient  == i.nom :
+                    _patient = i
+
+            docteur.recevoir(_cabinet,salle_attente,_patient)
+            docteur.diagnostic(_cabinet,_patient)
+            docteur.get_payed(_patient)
+            docteur.prescrire(_patient,_grille)
+            docteur.sortir(_cabinet,_patient,_salle)
+            _patient.se_deplacer(_salle,_pharmacie)
+            _patient.prendre_payer(_pharmacie,_grille)
+
+        else:
+            print(f"il ne reste plus personne, vous avez accueilli tous vos patients , vous avez gagné : {docteur.argent- 20000} € ajd !")
+            print(f"voici un recap de la journéee ")
+            
+            return False
+            
+
+# print(salle_attente.salle)
+# print(cabinet.salle)
+# docteur.recevoir(cabinet,salle_attente,sangoku)
+# print(salle_attente.salle)
+# print(cabinet.salle)
+# docteur.diagnostic(cabinet,sangoku)
+# docteur.get_payed(sangoku)
+# docteur.prescrire(sangoku,grille)
+
+# docteur.sortir(cabinet,sangoku,salle_attente)
+# sangoku.se_deplacer(salle_attente,pharmacie)
+# print(pharmacie.caisse,sangoku.argent)
+# print(sangoku.poche)
+# sangoku.prendre_payer(pharmacie,grille)
+# print(pharmacie.caisse,sangoku.argent)
+# print(sangoku.poche)
+# print(cimetierre.salle)
+
+medecin(cabinet,salle_attente,patients,grille,pharmacie)
+
 
 
 
